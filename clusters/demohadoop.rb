9d2adcbb09623ff98e999bf0c2021fbc722c834d
Hui Hu <huhui14@gmail.com>
2012-02-07 16:27:43 +0800

    upload new clusters/demohadoop.rb which works for CDH3 and vSphere 5.0

--- clusters/demohadoop.rb
+++ clusters/demohadoop.rb
@@ -1,43 +1,47 @@
 ClusterChef.cluster 'demohadoop' do
   setup_role_implications
-  mounts_ephemeral_volumes
+  # mounts_ephemeral_volumes
 
-  cloud do
-    backing             "ebs"
-    image_name          "maverick"
+  cloud :vsphere do
+    backing "instance"   # "ebs"
+    #image_name "oneiric"
+    image_name "maverick"
     availability_zones  ['us-east-1a']
   end
 
-  # role                  "big_package"
-  role                  "nfs_client"
-  recipe                "cluster_chef::dedicated_server_tuning"
+  #role "big_package"
+  #role "nfs_client"
+
+  cluster_role # add specific role for this cluster
 
   facet :master do
-    instances           1
-    cloud.flavor        "m1.small" # "m2.xlarge"
-    #
-    role                "hadoop"
-    role                "hadoop_namenode"
-    role                "hadoop_jobtracker"
-    role                "hadoop_initial_bootstrap"
-    role                "hadoop_tasktracker"
-    role                "hadoop_datanode"
+    instances 1
+    cloud.flavor "m1.small" # "m2.xlarge"
+    
+    facet_role # add specific role for this facet
+
+    role "hadoop"
+    role "hadoop_namenode"
+    role "hadoop_jobtracker"
+    role "hadoop_datanode"
+    role "hadoop_tasktracker"
   end
 
   facet :worker do
-    instances           2
-    cloud.flavor        "m1.small" # "m1.large"
-    #
-    role                "hadoop"
-    role                "hadoop_namenode"
-    role                "hadoop_jobtracker"
-    role                "hadoop_initial_bootstrap"
-    role                "hadoop_tasktracker"
-    role                "hadoop_datanode"
-    role                "hadoop_worker"
+    instances 2
+    cloud.flavor "m1.small" # "m1.large"
+
+    facet_role
+
+    role "hadoop"
+    role "hadoop_worker"
   end
 
   chef_attributes({
-      :cluster_size => facet('worker').instances,
-    })
+    :cluster_size => facet('worker').instances,
+    :hadoop => {
+      :dfs_replication => facet('worker').instances,
+    }
+  })
 end
+
